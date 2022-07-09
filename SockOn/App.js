db84060755170a2a4368dad8f9a29934ffcedd8e
Tan How Suen <howsuen@gmail.com>
2022-07-09 17:28:07 +0800

    Implemented Login system

--- SockOn/App.js
+++ SockOn/App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+import "react-native-url-polyfill/auto";
+import { useState, useEffect } from "react";
+import { supabase } from "./lib/supabase";
+import Login from "./components/Login";
+import Account from "./components/Account";
+import { View, StyleSheet } from "react-native";
+import { Session } from "@supabase/supabase-js";
+
+export default App = () => {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {session && session.user ? (
+        <Account key={session.user.id} session={session} />
+      ) : (
+        <Login />
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
   },
 });
