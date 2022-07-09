86c2aaf745eabc69eb9d32ff92fd802de2e3fb20
Giovanni Low <71576556+giovannilow@users.noreply.github.com>
2022-07-09 23:53:22 +0800

    Mountain Men

--- Frontend/components/HomeHeader.js
+++ Frontend/components/HomeHeader.js
@@ -1,95 +1,111 @@
 import React from "react";
-import { View, Text, Image, TextInput } from "react-native";
+import { View, Text, Image, TextInput, ImageBackground } from "react-native";
 
 import { COLORS, FONTS, SIZES, assets } from "../constants";
 
 const HomeHeader = ({ onSearch }) => {
   return (
-    <View
-      style={{
-        backgroundColor: COLORS.primary,
-        padding: SIZES.font,
-      }}
-    >
+    <View>
+      <ImageBackground
+          source={assets.mountain}
+          resizeMode="cover"
+          style={{ width: "100%", height: 300 }}
+        />
       <View
         style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
+          backgroundColor: COLORS.primary,
+          padding: SIZES.font,
         }}
       >
-        <Image
-          source={assets.logo}
-          resizeMode="contain"
-          style={{ width: 90, height: 25 }}
-        />
-
-        <View style={{ width: 45, height: 45 }}>
-          <Image
-            source={assets.person01}
-            resizeMode="contain"
-            style={{ width: "100%", height: "100%" }}
-          />
-          <Image
-            source={assets.badge}
-            resizeMode="contain"
-            style={{
-              position: "absolute",
-              width: 15,
-              height: 15,
-              bottom: 0,
-              right: 0,
-            }}
-          />
-        </View>
-      </View>
-
-      <View style={{ marginVertical: SIZES.font }}>
-        <Text
-          style={{
-            fontFamily: FONTS.bold,
-            fontSize: SIZES.extraLarge,
-            color: COLORS.white,
-            marginTop: SIZES.base / 2,
-          }}
-        >
-          HANOI, VIETNAM
-        </Text>
-        
-        <Text
-          style={{
-            fontFamily: FONTS.regular,
-            fontSize: SIZES.medium,
-            color: COLORS.white,
-          }}
-        >
-          4/3 - 9/3
-        </Text>
-
-      </View>
-
-      <View style={{ marginTop: SIZES.font }}>
         <View
           style={{
-            width: "100%",
-            borderRadius: SIZES.font,
-            backgroundColor: COLORS.gray,
             flexDirection: "row",
+            justifyContent: "center",
             alignItems: "center",
-            paddingHorizontal: SIZES.font,
-            paddingVertical: SIZES.small - 2,
+          }}
+        >
+          
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
           }}
         >
           <Image
-            source={assets.search}
+            source={assets.logo}
             resizeMode="contain"
-            style={{ width: 20, height: 20, marginRight: SIZES.base }}
+            style={{ width: 100, height: 45 }}
           />
-          <TextInput
-            placeholder="Search Documents"
-            style={{ flex: 1 }}
-            onChangeText={onSearch}
-          />
+
+          <View style={{ width: 45, height: 45 }}>
+            <Image
+              source={assets.person01}
+              resizeMode="contain"
+              style={{ width: "100%", height: "100%" }}
+            />
+            <Image
+              source={assets.badge}
+              resizeMode="contain"
+              style={{
+                position: "absolute",
+                width: 15,
+                height: 15,
+                bottom: 0,
+                right: 0,
+              }}
+            />
+          </View>
+        </View>
+
+        <View style={{ marginVertical: SIZES.font }}>
+          <Text
+            style={{
+              fontFamily: FONTS.bold,
+              fontSize: SIZES.extraLarge,
+              color: COLORS.white,
+              marginTop: SIZES.base / 2,
+            }}
+          >
+            KATHMANDU, NEPAL
+          </Text>
+          
+          <Text
+            style={{
+              fontFamily: FONTS.regular,
+              fontSize: SIZES.medium,
+              color: COLORS.white,
+            }}
+          >
+            4/3 - 9/3
+          </Text>
+
+        </View>
+
+        <View style={{ marginTop: SIZES.font }}>
+          <View
+            style={{
+              width: "100%",
+              borderRadius: SIZES.font,
+              backgroundColor: COLORS.gray,
+              flexDirection: "row",
+              alignItems: "center",
+              paddingHorizontal: SIZES.font,
+              paddingVertical: SIZES.small - 2,
+            }}
+          >
+            <Image
+              source={assets.search}
+              resizeMode="contain"
+              style={{ width: 20, height: 20, marginRight: SIZES.base }}
+            />
+            <TextInput
+              placeholder="Search Documents"
+              style={{ flex: 1 }}
+              onChangeText={onSearch}
+            />
+          </View>
         </View>
       </View>
     </View>
